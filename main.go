a05738f2ac13ece4af59946af90658347c49bbec
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-11-10 00:35:40 +0900

    Add how to evaluate into main.go

--- main.go
+++ main.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"fmt"
+	"go_type_inference/eval"
+	"go_type_inference/object"
 	"go_type_inference/parser"
 )
 
 func main() {
-	input := `if 2 < 3 then (if true then x else y) else false`
+	input := `if true then (if false then 3 else 2) else 3`
 
-	res := parser.Parse(input)
-	fmt.Println(res)
+	// Parse the input string
+	p := parser.Parse(input)
+
+	// Make Environment
+	env := object.NewEnvironment()
+
+	// Evaluate AST made from parser.Parse
+	e := eval.Eval(p, env)
+
+	fmt.Println(e.Inspect())
 }
