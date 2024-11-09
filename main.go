a6d341eef0bac36cf3c09634acc17a4a60c61e0f
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-11-09 12:29:07 +0900

    Add LOC showing how to parse

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"fmt"
+	"go_type_inference/parser"
+)
+
 func main() {
 	input := `if 2 < 3 then (if true then x else y) else false`
+
+	res := parser.Parse(input)
+	fmt.Println(res)
 }
