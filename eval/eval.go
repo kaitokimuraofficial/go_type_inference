9addaa8892101e8de4dcc0451efde8cf757291f1
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-11-11 16:26:51 +0900

    Use object.Object for eval_test.go

--- eval/eval.go
+++ eval/eval.go
@@ -13,27 +13,29 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		return evalStatement(node, env)
 	case ast.Declaration:
 		return evalDeclaration(node, env)
-	case ast.RecDeclaration:
-		return evalRecDeclaration(node, env)
 	case ast.Identifier:
 		obj, ok := env.Get(node.Value)
 		if !ok {
 			log.Fatal("Variable not bound")
 		}
+
 		switch obj := obj.(type) {
-		case *object.Integer:
-			return &object.Integer{Value: obj.Value}
-		case *object.Boolean:
-			return &object.Boolean{Value: obj.Value}
-		case *object.Function:
+		case object.Integer:
 			return obj
+		case object.Boolean:
+			return obj
+		case object.Function:
+			return obj
+		default:
+			log.Fatal("Undefined primitive!")
+			return nil
 		}
 	case ast.Integer:
-		return &object.Integer{Value: node.Value}
+		return object.Integer{Value: node.Value}
 	case ast.Boolean:
-		return &object.Boolean{Value: node.Value}
+		return object.Boolean{Value: node.Value}
 	case ast.FunExpr:
-		return &object.Function{Param: node.Param, Body: node.BodyExpr, Env: *env}
+		return object.Function{Param: node.Param, Body: node.BodyExpr, Env: *env}
 	case ast.BinOpExpr:
 		return evalBinOpExpr(node, env)
 	case ast.IfExpr:
@@ -59,13 +61,6 @@ func evalDeclaration(d ast.Declaration, env *object.Environment) object.Object {
 	return v
 }
 
-func evalRecDeclaration(rd ast.RecDeclaration, env *object.Environment) object.Object {
-	f := &object.Function{Param: rd.Param, Body: rd.BodyExpr, Env: *env}
-	env.Set(rd.Id.Value, f)
-
-	return f
-}
-
 func evalBinOpExpr(be ast.BinOpExpr, env *object.Environment) object.Object {
 	lv := Eval(be.Left, env)
 	rv := Eval(be.Right, env)
@@ -73,19 +68,19 @@ func evalBinOpExpr(be ast.BinOpExpr, env *object.Environment) object.Object {
 	switch be.Token.Type {
 	case token.PLUS:
 		if lv.Type() == object.INTEGER_OBJ && rv.Type() == object.INTEGER_OBJ {
-			return &object.Integer{Value: lv.(*object.Integer).Value + rv.(*object.Integer).Value}
+			return object.Integer{Value: lv.(object.Integer).Value + rv.(object.Integer).Value}
 		} else {
 			log.Fatal("Both arguments must be integer: +")
 		}
 	case token.ASTERISK:
 		if lv.Type() == object.INTEGER_OBJ && rv.Type() == object.INTEGER_OBJ {
-			return &object.Integer{Value: lv.(*object.Integer).Value * rv.(*object.Integer).Value}
+			return object.Integer{Value: lv.(object.Integer).Value * rv.(object.Integer).Value}
 		} else {
 			log.Fatal("Both arguments must be integer: *")
 		}
 	case token.LT:
 		if lv.Type() == object.INTEGER_OBJ && rv.Type() == object.INTEGER_OBJ {
-			return &object.Boolean{Value: lv.(*object.Integer).Value < rv.(*object.Integer).Value}
+			return object.Boolean{Value: lv.(object.Integer).Value < rv.(object.Integer).Value}
 		} else {
 			log.Fatal("Both arguments must be integer: <")
 		}
@@ -100,7 +95,7 @@ func evalIfExpr(ie ast.IfExpr, env *object.Environment) object.Object {
 	cnd := Eval(ie.Condition, env)
 
 	if cnd.Type() == object.BOOLEAN_OBJ {
-		if cnd.(*object.Boolean).Value {
+		if cnd.(object.Boolean).Value {
 			return Eval(ie.Consequence, env)
 		} else {
 			return Eval(ie.Alternative, env)
@@ -115,6 +110,7 @@ func evalIfExpr(ie ast.IfExpr, env *object.Environment) object.Object {
 func evalLetExpr(le ast.LetExpr, env *object.Environment) object.Object {
 	v := Eval(le.BindingExpr, env)
 	env.Set(le.Identifier.Value, v)
+
 	return Eval(le.BodyExpr, env)
 }
 
@@ -123,8 +119,8 @@ func evalAppExpr(ae ast.AppExpr, env *object.Environment) object.Object {
 	arg := Eval(ae.Argument, env)
 
 	if f.Type() == object.FUNCTION_OBJ {
-		env.Set(f.(*object.Function).Param.Value, arg)
-		return Eval(f.(*object.Function).Body, env)
+		env.Set(f.(object.Function).Param.Value, arg)
+		return Eval(f.(object.Function).Body, env)
 	}
 
 	log.Fatal("Non function value is applied: App")
@@ -133,7 +129,7 @@ func evalAppExpr(ae ast.AppExpr, env *object.Environment) object.Object {
 }
 
 func evalLetRecExpr(lr ast.LetRecExpr, env *object.Environment) object.Object {
-	f := &object.Function{Param: lr.Param, Body: lr.BindingExpr, Env: *env}
+	f := object.Function{Param: lr.Param, Body: lr.BindingExpr, Env: *env}
 	env.Set(lr.Id.Value, f)
 
 	return Eval(lr.BodyExpr, env)
--- eval/eval_test.go
+++ eval/eval_test.go
@@ -1,8 +1,11 @@
 package eval
 
 import (
+	"go_type_inference/ast"
 	"go_type_inference/object"
 	"go_type_inference/parser"
+	"go_type_inference/token"
+	"reflect"
 	"testing"
 )
 
@@ -11,95 +14,177 @@ func TestEval(t *testing.T) {
 
 	tests := map[string]struct {
 		input    string
-		expected string
+		expected object.Object
 	}{
 		"identifier": {
-			input:    "x",
-			expected: "10",
+			input: "x",
+			expected: object.Integer{
+				Value: 10,
+			},
 		},
 		"integer": {
-			input:    "2",
-			expected: "2",
+			input: "2",
+			expected: object.Integer{
+				Value: 2,
+			},
 		},
 		"boolean": {
-			input:    "true",
-			expected: "true",
+			input: "true",
+			expected: object.Boolean{
+				Value: true,
+			},
 		},
 		"binary operator(PLUS)": {
-			input:    "2 + 3",
-			expected: "5",
+			input: "2 + 3",
+			expected: object.Integer{
+				Value: 5,
+			},
 		},
 		"binary operator(PLUS) identifier": {
-			input:    "i + x",
-			expected: "11",
+			input: "i + x",
+			expected: object.Integer{
+				Value: 11,
+			},
 		},
 		"binary operator(ASTERISK)": {
-			input:    "2 * 3",
-			expected: "6",
+			input: "2 * 3",
+			expected: object.Integer{
+				Value: 6,
+			},
 		},
 		"binary operator(LT)": {
-			input:    "2 < 3",
-			expected: "true",
+			input: "2 < 3",
+			expected: object.Boolean{
+				Value: true,
+			},
 		},
 		"if true": {
-			input:    "if true then 2 else 3",
-			expected: "2",
+			input: "if true then 2 else 3",
+			expected: object.Integer{
+				Value: 2,
+			},
 		},
 		"if else": {
-			input:    "if false then 2 else 3",
-			expected: "3",
+			input: "if false then 2 else 3",
+			expected: object.Integer{
+				Value: 3,
+			},
 		},
 		"parentheses integer": {
-			input:    "(2 + 3)",
-			expected: "5",
+			input: "(2 + 3)",
+			expected: object.Integer{
+				Value: 5,
+			},
 		},
 		"nested if true": {
-			input:    "if (if false then true else false) then 2 else 3",
-			expected: "3",
+			input: "if (if false then true else false) then 2 else 3",
+			expected: object.Integer{
+				Value: 3,
+			},
 		},
 		"nested if identifier": {
-			input:    "if (if x < v then true else false) then 2 else i",
-			expected: "1",
+			input: "if (if x < v then true else false) then 2 else i",
+			expected: object.Integer{
+				Value: 1,
+			},
 		},
 		"let declaration": {
-			input:    "let x = 2",
-			expected: "2",
+			input: "let x = 2",
+			expected: object.Integer{
+				Value: 2,
+			},
 		},
 		"let in": {
-			input:    "let x = 2 in x + 3",
-			expected: "5",
+			input: "let x = 2 in x + 3",
+			expected: object.Integer{
+				Value: 5,
+			},
 		},
 		"nested let in": {
-			input:    "let x = 2 in let y = 3 in x + y",
-			expected: "5",
+			input: "let x = 2 in let y = 3 in x + y",
+			expected: object.Integer{
+				Value: 5,
+			},
 		},
 		"fun abstraction": {
-			input:    "fun x -> x + 3",
-			expected: "fun(x, (x + 3))",
+			input: "fun y -> y + 3",
+			expected: object.Function{
+				Param: ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "y"},
+					Value: "y",
+				},
+				Body: ast.BinOpExpr{
+					Token: token.Token{Type: token.PLUS, Literal: "+"},
+					Left: ast.Identifier{
+						Token: token.Token{Type: token.IDENT, Literal: "y"},
+						Value: "y",
+					},
+					Operator: "+",
+					Right: ast.Integer{
+						Token: token.Token{Type: token.INT, Literal: "3"},
+						Value: 3,
+					},
+				},
+				Env: object.Environment{
+					Store: map[string]object.Object{
+						"i": object.Integer{Value: 1},
+						"v": object.Integer{Value: 5},
+						"x": object.Integer{Value: 10},
+					},
+				},
+			},
 		},
 		"fun application-1": {
-			input:    "(fun x -> x + 3 ) 2",
-			expected: "5",
+			input: "(fun x -> x + 3 ) 2",
+			expected: object.Integer{
+				Value: 5,
+			},
 		},
 		"fun application-2": {
-			input:    "(fun param -> param + 3 ) 2",
-			expected: "5",
+			input: "(fun param -> param + 3 ) 2",
+			expected: object.Integer{
+				Value: 5,
+			},
 		},
 		"nested function application-1": {
-			input:    "(fun x -> (fun y -> x + y)) 2",
-			expected: "fun(y, (x + y))",
+			input: "(fun x -> (fun y -> x + y)) 2",
+			expected: object.Function{
+				Param: ast.Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "y"},
+					Value: "y",
+				},
+				Body: ast.BinOpExpr{
+					Token: token.Token{Type: token.PLUS, Literal: "+"},
+					Left: ast.Identifier{
+						Token: token.Token{Type: token.IDENT, Literal: "x"},
+						Value: "x",
+					},
+					Operator: "+",
+					Right: ast.Identifier{
+						Token: token.Token{Type: token.IDENT, Literal: "y"},
+						Value: "y",
+					},
+				},
+				Env: object.Environment{
+					Store: map[string]object.Object{
+						"i": object.Integer{Value: 1},
+						"v": object.Integer{Value: 5},
+						"x": object.Integer{Value: 2},
+					},
+				},
+			},
 		},
 		"nested function application-2": {
-			input:    "(fun x -> (fun y -> x + y)) 2 3",
-			expected: "5",
-		},
-		"let rec declaration": {
-			input:    "let rec f = fun n -> if n < 10 then 1 else n * f (n + 1)",
-			expected: "fun(n, if (n < 10) then ( 1 ) else ( (n * ( f, (n + 1) ) ) ) )",
+			input: "(fun x -> (fun y -> x + y)) 2 3",
+			expected: object.Integer{
+				Value: 5,
+			},
 		},
 		"let rec expression": {
-			input:    "let rec fact = fun n -> (if 9 < n then 1 else n * (fact (n+1))) in fact 8",
-			expected: "72",
+			input: "let rec fact = fun n -> (if 9 < n then 1 else n * (fact (n+1))) in fact 8",
+			expected: object.Integer{
+				Value: 72,
+			},
 		},
 	}
 
@@ -107,17 +192,17 @@ func TestEval(t *testing.T) {
 		tt := tt
 
 		env := object.NewEnvironment()
-		env.Set("i", &object.Integer{Value: 1})
-		env.Set("v", &object.Integer{Value: 5})
-		env.Set("x", &object.Integer{Value: 10})
+		env.Set("i", object.Integer{Value: 1})
+		env.Set("v", object.Integer{Value: 5})
+		env.Set("x", object.Integer{Value: 10})
 
 		t.Run(name, func(t *testing.T) {
 			t.Parallel()
 
-			got := Eval(parser.Parse(tt.input), env).Inspect()
+			got := Eval(parser.Parse(tt.input), env)
 
-			if got != tt.expected {
-				t.Errorf("expect: %s, but got: %s", tt.expected, got)
+			if !reflect.DeepEqual(got, tt.expected) {
+				t.Errorf("expect: %q, but got: %q", tt.expected, got)
 			}
 		})
 	}
