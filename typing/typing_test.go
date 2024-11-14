f3ad9de2b4fcfcb28fd091ba461610cba209bb2f
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-11-14 13:25:39 +0900

    Refactor and change to use go-cmp in typing_test.go

--- typing/typing_test.go
+++ typing/typing_test.go
@@ -2,10 +2,10 @@ package typing
 
 import (
 	"go_type_inference/ast"
-	"go_type_inference/object"
 	"go_type_inference/parser"
-	"reflect"
 	"testing"
+
+	"github.com/google/go-cmp/cmp"
 )
 
 func TestInfer(t *testing.T) {
@@ -14,100 +14,101 @@ func TestInfer(t *testing.T) {
 	testCases := []struct {
 		name  string
 		input string
-		want  object.InferredObject
+		want  Type
 	}{
 		{
 			name:  "identifier",
 			input: "i",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
 			name:  "integer",
 			input: "2",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
 			name:  "boolean",
 			input: "true",
-			want:  object.TyBool{},
+			want:  &TyBool{},
 		},
 		{
-			name:  "binary operator(PLUS)",
+			name:  "add two integers",
 			input: "2 + 3",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
-			name:  "binary operator(PLUS) identifier",
+			name:  "add two identifiers",
 			input: "i + v",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
-			name:  "binary operator(ASTERISK)",
+			name:  "multiple two integers",
 			input: "2 * 3",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
-			name:  "binary operator(LT)",
+			name:  "compare two integers",
 			input: "2 < 3",
-			want:  object.TyBool{},
+			want:  &TyBool{},
 		},
 		{
-			name:  "if true",
-			input: "if true then 2 else 3",
-			want:  object.TyInt{},
-		},
-		{
-			name:  "if else",
-			input: "if false then 2 else 3",
-			want:  object.TyInt{},
-		},
-		{
-			name:  "parentheses integer",
+			name:  "parenthesized expression",
 			input: "(2 + 3)",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
-			name:  "nested if true",
+			name:  "if true expression",
+			input: "if true then 2 else 3",
+			want:  &TyInt{},
+		},
+		{
+			name:  "if else expression",
+			input: "if false then 2 else 3",
+			want:  &TyInt{},
+		},
+		{
+			name:  "nested if expression",
 			input: "if (if false then true else false) then 2 else 3",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
-			name:  "nested if identifier",
+			name:  "nested if expression with identifiers in environment",
 			input: "if (if i < v then true else false) then 2 else i",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
 			name:  "let declaration",
 			input: "let x = 2",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
-			name:  "let in",
+			name:  "let expression",
 			input: "let x = 2 in x + 3",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 		{
-			name:  "nested let in",
+			name:  "nested let expression",
 			input: "let x = 2 in let y = 3 in x + y",
-			want:  object.TyInt{},
+			want:  &TyInt{},
 		},
 	}
 
 	for _, tc := range testCases {
 		tc := tc
 
-		env := object.NewTypeEnvironment()
-		env.Set(ast.Identifier{Value: "b"}, object.TyBool{})
-		env.Set(ast.Identifier{Value: "i"}, object.TyInt{})
-		env.Set(ast.Identifier{Value: "v"}, object.TyInt{})
+		env := &Environment{Store: make(map[ast.Identifier]Type)}
+		env.Set(ast.Identifier{Value: "b"}, &TyBool{})
+		env.Set(ast.Identifier{Value: "i"}, &TyInt{})
+		env.Set(ast.Identifier{Value: "v"}, &TyInt{})
 
 		t.Run(tc.name, func(t *testing.T) {
 			t.Parallel()
 
-			got := Infer(parser.Parse(tc.input), env)
+			p := parser.Parse(tc.input)
+			got := Infer(p, env)
 
-			if !reflect.DeepEqual(got, tc.want) {
-				t.Errorf("expect: %s, but got: %s", tc.want, got)
+			if diff := cmp.Diff(tc.want, got); diff != "" {
+				t.Errorf("Infer(%q) returned unexpected difference (-want +got):\n%s", tc.input, diff)
 			}
 		})
 	}
