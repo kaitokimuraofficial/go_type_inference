3c2c5d04f53fde2a84b1b15f601cceef778272f9
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-11-14 13:24:10 +0900

    Organized object package into purpose-specific package

---
+++ eval/value.go
@@ -0,0 +1,27 @@
+package eval
+
+import "go_type_inference/ast"
+
+type Value interface {
+	value()
+}
+
+type (
+	Integer struct {
+		Value int
+	}
+
+	Boolean struct {
+		Value bool
+	}
+
+	Function struct {
+		Param ast.Identifier
+		Body  ast.Expr
+		Env   Environment
+	}
+)
+
+func (*Integer) value()  {}
+func (*Boolean) value()  {}
+func (*Function) value() {}
