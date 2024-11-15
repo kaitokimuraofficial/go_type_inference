15d142137277cf98c4d81fb74fc50c759f8918f7
kaitokimuraofficial <k.kaito0422@akane.waseda.jp>
2024-11-15 20:09:58 +0900

    Change Environment Value Type to Scheme

--- typing/environment.go
+++ typing/environment.go
@@ -3,15 +3,15 @@ package typing
 import "go_type_inference/ast"
 
 type Environment struct {
-	Store map[ast.Identifier]Type
+	Store map[ast.Identifier]Scheme
 }
 
-func (e *Environment) Get(k ast.Identifier) (Type, bool) {
+func (e *Environment) Get(k ast.Identifier) (Scheme, bool) {
 	obj, ok := e.Store[k]
 	return obj, ok
 }
 
-func (e *Environment) Set(k ast.Identifier, v Type) Type {
+func (e *Environment) Set(k ast.Identifier, v Scheme) Scheme {
 	e.Store[k] = v
 	return v
 }
